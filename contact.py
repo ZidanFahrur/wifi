74bf5cf482f485c35be81e07869f59dd557920fb
ZidanFahrur <107808695+ZidanFahrur@users.noreply.github.com>
2022-06-20 20:51:00 +0700

    Update contact.py

--- contact.py
+++ contact.py
@@ -4,7 +4,7 @@ os.system("termux-contact-list > /dev/null > list.json")
 op = open("list.json","r").read()
 j = json.loads(str(op))
 print ("Contact List With CLI")
-print ("copyright TigerPo")
+print ("copyright 2019 TigerPo")
 for contact in j:
 	print ("[+] Name: "+contact["name"])
 	print ("[+] Number: "+contact["number"])
